312fe1df21a377a13c7c6e6a7f0fe62272c9872d
arnaud de valbray <avalbray@gmail.com>
2017-11-24 12:09:40 +0100

    migrations

--- db/migrate/20171123164239_create_doses.rb
+++ db/migrate/20171123164239_create_doses.rb
@@ -1,4 +1,4 @@
-class CreateDoses < ActiveRecord::Migration[5.1]
+class CreateDoses < ActiveRecord::Migration[5.0]
   def change
     create_table :doses do |t|
       t.references :ingredient, foreign_key: true
